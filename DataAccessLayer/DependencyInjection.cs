ed7641fc604046f3f394f01a832e0926d5eaab9e
saniu <saniulnaim@gmail.com>
2026-05-13 11:43:18 +0600

    Add MongoDB support with DI and config enhancements
    Added MongoDB.Driver package and configured dependency injection for IMongoClient and IMongoDatabase using a connection string from appsettings.json. The connection string now supports environment variable substitution for host and port. Updated appsettings.json to include a MongoDB connection string template.

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +11,18 @@ namespace DataAccessLayer
     {
        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionStringTemplate = configuration.GetConnectionString("MongoDB")!;
+            string connectionString =  connectionStringTemplate
+                .Replace("{$MONGODB_HOST}", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+                .Replace("{$MONGODB_PORT}", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+
+            services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+            services.AddScoped<IMongoDatabase>(provider =>
+            {
+                var client = provider.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("OrdersDatabase");
+            });
+
             return services;
         }
     }
